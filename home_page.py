ffe5d0ef2a9a60e7a692cd532d3969c38668b1a7
Kashp258 <125690924+Kashp258@users.noreply.github.com>
2024-10-15 01:33:58 +0530

    Create home_page.py

---
+++ home_page.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def show_home_page():
+    st.title("Welcome to Waste Classification App")
+    st.write("""
+        This is a multi-page web application that helps classify waste 
+        and provides step-by-step suggestions on how to recycle, reuse, 
+        or dispose of waste properly.
+    """)
