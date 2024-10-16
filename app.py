c4b620a8985e34093af5495cfded48678d0083e2
Kashp258 <125690924+Kashp258@users.noreply.github.com>
2024-10-16 14:57:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,17 @@ from home_page import show_home_page
 from classification_page import show_classification_page
 from about_page import show_about_page
 
-# Create a sidebar for navigation
-st.sidebar.title("Navigation")
-page = st.sidebar.radio("Go to", ["Home", "Classification", "About"])
+# Main function to navigate through the pages
+def main():
+    st.sidebar.title("Navigation")
+    page = st.sidebar.radio("Go to", ("Home", "Classification", "About"))
 
-# Render the selected page
-if page == "Home":
-    show_home_page()
-elif page == "Classification":
-    show_classification_page()
-elif page == "About":
-    show_about_page()
+    if page == "Home":
+        show_home_page()
+    elif page == "Classification":
+        show_classification_page()
+    elif page == "About":
+        show_about_page()
+
+if __name__ == "__main__":
+    main()
